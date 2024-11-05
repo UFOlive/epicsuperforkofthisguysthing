406e181e436a869734c9a298fd4d548aa8ca460b
UFOlive <132823284+UFOlive@users.noreply.github.com>
2024-11-05 09:57:15 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -168,7 +168,7 @@ window.onload = () => { // FUNCAO A SER EXECUTADA QUANDO A JANELA CARREGAR
             this.size = 30;
             this.gridY = Math.floor((this.y + this.size / 2) / gridHeigth);
             this.gridX = Math.floor((this.x + this.size / 2) / gridWidth);
-            this.bombs = 1;
+            this.bombs = 999;
             this.bombPower = 1;
             this.health = health;
             this.right = this.up = this.right = false;
